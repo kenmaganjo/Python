af4a1dcb91ef714da9082f0b24a3b9931054eab4
kenmaganjo <maganjokennedy@gmail.com>
2023-04-30 17:50:38 +0300

    return largest number

--- task5.py
+++ task5.py
@@ -1,21 +1,22 @@
 #Implement a program that takes 3 form inputs or from the terminal, and stores them in three variables. Return the largest of the three.
-num1 = float(input('Enter a number: '))
-num2 = float(input('Enter a number: '))
-num3 = float(input('Enter a number: '))
-if num1 > num2 and num1 > num3:
-    print("Largest number =", num1)
-else:
-    if num2 > num1 and num2 > num3:
-        print("Largest number =", num2)
+def return_largest(a,b,c):
+    if num1 > num2 and num1 > num3:
+        return num1    
     else:
-        print("Largest number =", num3)
+        if num2 > num1 and num2 > num3:
+            return num2      
+        else:
+            return num3
 
-    # ans:
-    #     Enter a number: 123.254
-    #     Enter a number: 500
-    #     Enter a number: 20254.214
-
-    #     Largest number = 20254.214
     
-        
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+num3 = float(input('Enter the third number: '))  
+
+number = return_largest(num1,num2,num3)
+
+
+print("Largest number =", number)
+
+
         
