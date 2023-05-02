c4f3eb494192e5ea223ca11886fb3b8d490dc75f
kenmaganjo <maganjokennedy@gmail.com>
2023-05-02 16:32:20 +0300

    student marks grading

--- task7.py
+++ task7.py
@@ -1,31 +1,31 @@
 #Write that prompts the user to input student marks. The input should be between 0 and 100.Then output the correct grade: 
-try:
-    marks = float(input("Enter student marks: "))
-    if marks < 0 or marks > 100:
-        print("Inavlid marks")
-    else:
-        if marks > 79:
-            print("Grade: A")
+def marks_grading(): 
+    try:
+        
+        if marks < 0 or marks > 100:
+            return "Inavlid marks"
         else:
-            if marks >= 60 and marks <= 79:
-                print("Grade: B-")
+            if marks > 79:
+                return "Grade: A"
             else:
-                if marks >= 49 and marks <= 59:
-                    print("Grade: C-")
+                if marks >= 60 and marks <= 79:
+                    return "Grade: B-"
                 else:
-                    if marks >= 40 and marks <= 49:
-                        print("Grade: D")
+                    if marks >= 49 and marks <= 59:
+                        return "Grade: C-"
                     else:
-                        print("Grade: E")
-except Exception as x:
-    print(x)
+                        if marks >= 40 and marks < 49:
+                            return "Grade: D"
+                        else:
+                            return "Grade: E"
+                            
+    except Exception as x:
+        return(x)
 
-    # ans:
-    #     Enter student marks: 55
-    #     Grade: C-
-    #     Enter student marks: 39
-    #     Grade: E
-    #     Enter student marks: -8
-    #     Inavlid marks
-    #     Enter student marks: 108
-    #     Inavlid marks
+marks = float(input("Enter student marks: ")) 
+
+marks = marks_grading()
+
+# print(marks)
+
+    
