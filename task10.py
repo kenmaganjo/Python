78028c1e1ea3c0cf57bcbdd233ad4a864bb1cf8b
kenmaganjo <maganjokennedy@gmail.com>
2023-05-03 12:35:13 +0300

    total stock calculated

--- task10.py
+++ task10.py
@@ -1,8 +1,12 @@
 #Write a program that calculates the total stock in a company from the array/list below if we know that the stock is the last digit in every array/list.
-prods = [['omo','30kshs',300], ['milk','50kshs',200],['bread','45kshs',359], ['coffee','5kshs',79]]
-total_stock = 0
-for i in prods:
-        total_stock = total_stock + i[2]
-print(total_stock)
+def stock_total():
+        prods = [['omo','30kshs',300], ['milk','50kshs',200],['bread','45kshs',359], ['coffee','5kshs',79]]
+        total_stock = 0
+        for i in prods:
+                total_stock = total_stock + i[2]
 
-# ans: 938
+        return total_stock
+
+stock = stock_total()
+
+print(stock)
