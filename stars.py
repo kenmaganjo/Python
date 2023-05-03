2c01cdd0a2fdf774258bc5a814e7fce9414354a1
kenmaganjo <maganjokennedy@gmail.com>
2023-05-03 12:24:45 +0300

    stars

--- stars.py
+++ stars.py
@@ -1,4 +1,6 @@
 #Write a program called stars.py. It should prompt the user for a number, and it should print the number of stars till the number entered.
 num = int(input("Enetr a number: "))
 for i in range(num):
-   print("*" * (i +1))
+    print("*" * (i +1))
+
+
