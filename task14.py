f0b8c25d57ce3d7fcf33efdaa368d05236c7e6c9
kenmaganjo <maganjokennedy@gmail.com>
2023-05-03 13:09:26 +0300

    sum of numbers only

--- task14.py
+++ task14.py
@@ -1,28 +1,18 @@
 #Write a program that takes input of 2 values and adds them. The program should only accept numbers and floats only or otherwise display an error “invalid character entered” and take the user to re-enter the inputs .
-while True:
+for i in range(3):
     try:
         num1 = float(input("Enter a number: "))
         num2 = float(input("Enter a number: "))
         sum = num1 + num2
+
         print("Sum =", sum)
         break
 
-        
+            
     except ValueError:
         print("Invalid character entered. Enter numbers or floats only.")
 
 
-        # ans:
-        #     Enter a number: 12.65
-        #     Enter a number: 45
-        #     Sum = 57.65
 
-        #     Enter a number: rg
-        #     Invalid character entered. Enter numbers or floats only.
-        #     Enter a number: 6
-        #     Enter a number: #
-        #     Invalid character entered. Enter numbers or floats only.
-        #     Enter a number: /
-        #     Invalid character entered. Enter numbers or floats only.
-        #     Enter a number: 
+       
 
